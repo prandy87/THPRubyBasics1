eca4ea37a646854d03dcc24a9f930ee2aecef5d8
Pascal Mercat <pascal.mercat@gmail.com>
2018-07-05 00:27:44 +0200

    A game to climb stairs with a dice.

--- climbstairs.rb
+++ climbstairs.rb
@@ -13,11 +13,21 @@
 
 
 def staircase
-    puts "Here is a staircase: "
-    puts "You are at step 0. " 
+    puts " "
+    puts "Here is a staircase:"
+    puts " "
+    puts "You are at step 0." 
+    puts " "
+    puts "Throw a dice to move up or down the stairs."
+    puts " "
     puts "If dice is 1, you go back 1 step."
+    puts " "
     puts "If dice is 2, 3 or 4, you stay put."
+    puts " "
     puts "If dice is 5 or 6, you go forward 1 step."
+    puts " "
+    puts "       **************************"
+    puts " "
            
     i = 1
     str = " "
@@ -48,30 +58,60 @@ end
 def move
     launch_dice
     
+
     if @dice_face == 5 || @dice_face == 6
         @pos += 1
-        puts "You go forward! You are at rank #{@pos}."
+        puts "You go forward! You are now at step #{@pos}."
         #return @pos
-    elsif @dice_face == 2 || @dice_face == 3 || @dice_face == 4 ||
-        @pos == @pos
-        puts "You stay put. You are at rank #{@pos}."
+    end
+    if @dice_face == 2 || @dice_face == 3 || @dice_face == 4
+        puts "You stay put. You are at step #{@pos}."
         #return @pos
-    elsif @dice_face == 1 
-        @pos -= 1
-        puts "You go back. You are at rank #{@pos}."
+    end
+    if @dice_face == 1
+        if @pos == 0
+            @pos = 0
+            puts "You stay put. You are at step #{@pos}."
+        end
+        if @pos > 1
+            @pos = @pos - 1
+            puts "You go back. You are now at step #{@pos}."
+        end
+        
         #return @pos
     end
     
+    
 end
 
-def win
+
+def play 
+    
     @pos = 0
     i = 0
+    
     while @pos < 10
-        move
-        i += 1
+        
+        puts "
+        Type 't'to roll the dice."
+        cmd = gets.chomp    
+        if cmd == 't'
+            puts " "
+            move
+            i += 1
+        else
+            puts "Wrong command!"
+
+        end
     end
-    puts "YES !! You are at the TOP in #{i} iterations of throwing dice !"
+    if @pos == 10  
+        puts " "
+        puts "YES !! You got to the TOP after #{i} tosses of the dice !"
+    end
+        
 end
 
-win
+
+play
+
+
