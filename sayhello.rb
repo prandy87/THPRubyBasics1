f0e5fa8ea72038f979df5a5b513cccf1f93eeda0
Pascal Mercat <pascal.mercat@gmail.com>
2018-07-04 20:56:56 +0200

    Create First exos

---
+++ sayhello.rb
@@ -0,0 +1,6 @@
+def say_hello
+    puts "*sigh*.... Hello, world..."
+end
+
+
+say_hello
---
+++ yourname.rb
@@ -0,0 +1,7 @@
+def your_name_input
+    puts "what is your name ?"
+    i = gets.chomp
+    puts "hello, " + i 
+end
+
+your_name_input
