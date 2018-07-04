d5810527ebb891c29d624a2ec718df7ab50fee70
Pascal Mercat <pascal.mercat@gmail.com>
2018-07-05 01:07:31 +0200

    A game to climb stairs with a dice.

--- climbstairs.rb
+++ climbstairs.rb
@@ -2,15 +2,12 @@
 
 #This short game is part of the THP Beginner Ruby Program exos. 
 #Get to the top!
-#Need to implement manuel dice throw.
+
 
 ############################################################################
 
 
 #Generate Stairs
-
-
-
 
 def staircase
     puts " "
@@ -39,49 +36,41 @@ def staircase
     end
 end
 
+
 def perform
     staircase
 end
 
+
 perform 
 
-
-#Auto game
 
 def launch_dice
     @dice_face = (rand(1 .. 6).to_i)
     puts "Dice value is #{@dice_face}"
-    
-    
 end
 
 def move
+    
     launch_dice
     
-
     if @dice_face == 5 || @dice_face == 6
         @pos += 1
         puts "You go forward! You are now at step #{@pos}."
-        #return @pos
     end
     if @dice_face == 2 || @dice_face == 3 || @dice_face == 4
         puts "You stay put. You are at step #{@pos}."
-        #return @pos
     end
     if @dice_face == 1
         if @pos == 0
             @pos = 0
             puts "You stay put. You are at step #{@pos}."
         end
-        if @pos > 1
+        if @pos > 0
             @pos = @pos - 1
             puts "You go back. You are now at step #{@pos}."
         end
-        
-        #return @pos
     end
-    
-    
 end
 
 
@@ -89,9 +78,7 @@ def play
     
     @pos = 0
     i = 0
-    
     while @pos < 10
-        
         puts "
         Type 't'to roll the dice."
         cmd = gets.chomp    
@@ -101,14 +88,12 @@ def play
             i += 1
         else
             puts "Wrong command!"
-
         end
     end
     if @pos == 10  
         puts " "
         puts "YES !! You got to the TOP after #{i} tosses of the dice !"
     end
-        
 end
 
 
